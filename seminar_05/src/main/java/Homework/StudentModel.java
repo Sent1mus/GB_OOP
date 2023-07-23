32fb11b24958d07bd558d2b6e5489ce18cfe3c7c
Elim Yunusov <elim.yunusov1@gmail.com>
2023-07-23 23:45:02 +0400

    seminar_05

---
+++ seminar_05/src/main/java/Homework/StudentModel.java
@@ -0,0 +1,18 @@
+package Homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class StudentModel {
+        private List<Student> students = new ArrayList<>();
+
+        public void create(String name, UUID groupId) {
+            students.add(new Student(name, groupId));
+        }
+
+        public List<Student> getAll() {
+            return List.copyOf(students);
+        }
+
+    }
